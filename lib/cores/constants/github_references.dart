98234110fd05d38fa5c4117988829a5372b773a3
Joseph Chingalo <profschingalo@gmail.com>
2026-01-20 11:46:51 +0300

    chore: update app reference

--- lib/cores/constants/github_references.dart
+++ lib/cores/constants/github_references.dart
@@ -1,7 +1,7 @@
 class GitHubReferences {
   //TODO Replace with correctly informations
-  static const String owner = 'hisptz';
-  static const String repo = 'timr-public-release';
+  static const String owner = 'chingalo';
+  static const String repo = 'auto_update_app';
   static const String apkKey = 'prod';
   static const String token = '';
 }
